87e8daf2eb415cdea4b259d07b66ee7bfe73aa80
준오 박 <jun5gigy12@gmail.com>
2021-10-12 22:04:20 +0900

    Add function(PAPAGO)

--- TesseractExample/src/tess4j/example/Main.java
+++ TesseractExample/src/tess4j/example/Main.java
@@ -1,23 +1,130 @@
 package tess4j.example;
 
 import java.io.File;
+
 import net.sourceforge.tess4j.*;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         File cwd = new File(".");
+        String result = "";
         String cwdPath = cwd.getAbsolutePath();
-        File imageFile = new File(cwdPath + "\\src\\img\\eurotext.png"); // Path to the target image file.
+        File imageFile = new File(cwdPath + "\\src\\img\\K-009.png"); // Path to the target image file.
         ITesseract instance = new Tesseract(); // JNA Interface Mapping.
         instance.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata"); // Path to the tesseract data files.
 
         try {
-            String result = instance.doOCR(imageFile);
+            result = instance.doOCR(imageFile);
             System.out.println(result);
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
+        }
+
+        //파파고 번역
+        String clientId = "_mLgROuXC_v0ByKVfrXv";// 애플리케이션 클라이언트 아이디값";
+        String clientSecret = "u7XMv1IeKY";// 애플리케이션 클라이언트 시크릿값";
+
+        String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+        String text;
+        try {
+            text = URLEncoder.encode(result, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("인코딩 실패", e);
+        }
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+
+        String responseBody = post(apiURL, requestHeaders, text);
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(responseBody);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        JSONObject objMessage = (JSONObject) jsonObject.get("message");
+        JSONObject objResult = (JSONObject) objMessage.get("result");
+        String translatedText = (String) objResult.get("translatedText");
+
+        System.out.println(translatedText);
+
+    }
+
+    private static String post(String apiUrl, Map<String, String> requestHeaders, String text) {
+        HttpURLConnection con = connect(apiUrl);
+        String postParams = "source=en&target=ko&text=" + text; // 원본언어: 영어 (en) -> 목적언어: 한국어 (ko)
+        try {
+            con.setRequestMethod("POST");
+            for (Map.Entry<String, String> header : requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postParams.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else { // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl) {
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection) url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body) {
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
         }
     }
 }
