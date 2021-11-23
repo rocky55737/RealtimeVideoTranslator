da75fad026edf085e0434ce25abf70898b29298c
록기 <rocky55737@gmail.com>
2021-11-23 19:27:11 +0900

    ExitButton

--- translator/main/src/org/realtimetranslator/displayer/DisplayerGUI.java
+++ translator/main/src/org/realtimetranslator/displayer/DisplayerGUI.java
@@ -1,18 +1,33 @@
 package org.realtimetranslator.displayer;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
 
 public class DisplayerGUI {
     JFrame jFrame;
     JLabel jLabel;
+    JButton jButton;
 
     public DisplayerGUI(){
         this.jFrame = new JFrame("translation result");
+        this.jFrame.setLayout(new GridLayout(2,1));
         this.jLabel = new JLabel();
+        this.jButton = new JButton("종료");
+        this.jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
         this.jFrame.add(jLabel);
+        this.jFrame.add(jButton);
         this.jFrame.setVisible(true);
     }
     public void display(String displayText){
-        this.jFrame.setSize(displayText.length()*10, 100);
+        this.jFrame.setSize(displayText.length()*10, 200);
         this.jLabel.setText(displayText);
     }
+
+
 }
